e029d2d8acb256767d8babc47d9fb2b674555161
Andrey Zhartun <qeetree@gmail.com>
2020-04-05 11:00:33 +0600

    Updates

---
+++ precache-manifest.10e968f7163322ba2538a21b2ad1438c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5a62a15a98618655d331aa0804103d",
+    "url": "/point-the-flag/index.html"
+  },
+  {
+    "revision": "82d96123e7804d590088",
+    "url": "/point-the-flag/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1457e2e28561e8617298",
+    "url": "/point-the-flag/static/css/main.b16312bc.chunk.css"
+  },
+  {
+    "revision": "82d96123e7804d590088",
+    "url": "/point-the-flag/static/js/2.e17d5586.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/point-the-flag/static/js/2.e17d5586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1457e2e28561e8617298",
+    "url": "/point-the-flag/static/js/main.2c23b095.chunk.js"
+  },
+  {
+    "revision": "bc89e5c82b0c1a6081d9",
+    "url": "/point-the-flag/static/js/runtime-main.8c5c7dd0.js"
+  }
+]);
