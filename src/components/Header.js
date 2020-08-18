0f5a785eb653addfaf89fd29eae5e034146fb73a
Andrey Zhartun <qeetree@gmail.com>
2020-08-18 10:18:58 +0600

    переход с bootstrap на чистый css в Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,36 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarText, NavItem, NavLink, Nav } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFlag, faQuestion, faAddressCard, faCode } from '@fortawesome/free-solid-svg-icons';
+import '../styles/Header.css';
 
 class Header extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar dark>
-          <Nav className="mr-auto" style={{ flexDirection: "row" }} navbar>
-            <NavItem active>
-              <NavLink style={{ padding: "0.5rem" }}>
+        <nav>
+          <ul navbar>
+            <li>
+              <span>
                 <FontAwesomeIcon icon={faFlag} />
                 <FontAwesomeIcon icon={faQuestion} />
                 {" Укажите страну по ее флагу"}
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink style={{ padding: "0.5rem" }} href="https://github.com/AndreyZhartun/point-the-flag">
+              </span>
+            </li>
+            <li>
+              <a href="https://github.com/AndreyZhartun/point-the-flag">
                 <FontAwesomeIcon icon={faCode} />
                 {" Github "}
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/">
+              </a>
+            </li>
+            <li>
+              <a href="/">
                 <FontAwesomeIcon icon={faAddressCard} />
                 {" Про меня"}
-              </NavLink>
-            </NavItem>
-          </Nav>
-          <NavbarText>React, leaflet.js, Nominatim API</NavbarText>
-        </Navbar>
+              </a>
+            </li>
+          </ul>
+          <span>React, leaflet.js, Nominatim API</span>
+        </nav>
       </React.Fragment>
     );
   }
