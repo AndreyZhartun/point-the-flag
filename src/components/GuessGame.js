8c41907868b4f062e3036cae8d66cd2130ee2fdb
Andrey Zhartun <qeetree@gmail.com>
2020-08-19 15:35:45 +0600

    больше адаптивности в Game, контроль размера img

--- src/components/GuessGame.js
+++ src/components/GuessGame.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapMarkerAlt, faFlag, faCheck, faQuestion } from '@fortawesome/free-solid-svg-icons';
+import { faMapMarkerAlt, faFlag, faCheck, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import "../styles/GuessGame.css";
 
 import { connect } from 'react-redux';
-import { /*changeCurrentFlag,*/ fetchAddress, setRandomFirstIndex } from '../redux/ActionCreators';
-import "../styles/GuessGame.css";
+import { fetchAddress, setRandomFirstIndex } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
   return {
@@ -18,7 +18,6 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => ({
-  //changeCurrentFlag: () => { dispatch(changeCurrentFlag()) },
   fetchAddress: () => { dispatch(fetchAddress()) },
   setRandomFirstIndex: () => { dispatch(setRandomFirstIndex()) }
 });
@@ -37,11 +36,13 @@ class GuessGame extends Component {
   }
 
   render() {
+    //localhost ...src={require(`../../public/${this.props.flags[this.props.game.currentFlagIndex].path}`)}
     return (
       <div className="game-card">
 
         <div className="row">
           <div className="col-img">
+
             <img src={this.props.flags[this.props.game.currentFlagIndex].path} className="flag-img" alt="[Текущий флаг]" />
           </div>
           <div className="col-lead hide-on-mobile">
@@ -67,23 +68,33 @@ class GuessGame extends Component {
             "  " + this.props.marker.lat.toFixed(2) + ", " + this.props.marker.lng.toFixed(2) :
             "Маркер не найден"}
         </p>
-        <p hidden={!this.props.prevCountryMessage}>
-          <FontAwesomeIcon icon={faQuestion} />
-          <span className="hide-on-mobile">{"Предыдущий флаг: "}</span>
-          <img src={this.props.game.shownFlags.length > 0 ?
-            this.props.flags[this.props.game.shownFlags[this.props.game.shownFlags.length - 1]].path
-            : 'temp_path'}
-            className="flag-img-mini" alt="[Флаг]" />
-          {" " + this.props.prevCountryMessage}
-        </p>
         <button
           type="button"
           className="button"
           color="primary"
           disabled={this.props.requestSent || !this.props.game.isInProgress}
           onClick={this.handleConfirmation}>
-          Подтвердить <span className="hide-on-mobile">выбранные координаты</span>
+          <span className="hide-on-mobile">Подтвердить выбранные координаты</span>
+          <span className="hide-on-desktop">
+            <FontAwesomeIcon icon={faMapMarkerAlt} />
+            &nbsp;
+            <FontAwesomeIcon icon={faCheck} />
+          </span>
         </button>
+        <p hidden={!this.props.prevCountryMessage}>
+          <hr />
+          <span className="hide-on-desktop">
+            <FontAwesomeIcon icon={faArrowLeft} />
+            <FontAwesomeIcon icon={faFlag} />
+          </span>
+          <span className="hide-on-mobile">{"Предыдущий флаг: "}</span>
+          <img src={this.props.game.shownFlags.length > 0 ?
+            this.props.flags[this.props.game.shownFlags[this.props.game.shownFlags.length - 1]].path
+            : 'temp_path'}
+            className="flag-img-mini" alt="[Флаг]" />
+          <br />
+          {" " + this.props.prevCountryMessage}
+        </p>
       </div>
     )
   }
